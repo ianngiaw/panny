5cb738a86b0ca37eb3a187b083450e86af22accc
Ian Ngiaw <ianngiawtingan@gmail.com>
2016-09-10 16:38:34 -0700

    Fix get next flight data

--- src/utils/sdk.js
+++ src/utils/sdk.js
@@ -26,14 +26,16 @@ export const getTravelTime = () => {
   });
 };
 
-// Getting an error, will return to it later
-// export const getNextFlightData = () => {
-//   return new Promise(resolve => {
-//     inflight.initService('connecting_gate/v1', nextFlightData => {
-//       nextFlightData.connectingGateInfo((err, data) => {
-//         console.log(data); // eslint-disable-line
-//         resolve(data);
-//       });
-//     });
-//   });
-// };
+export const getNextFlightData = () => {
+  return new Promise(resolve => {
+    inflight.initService('connecting_gate/v1', nextFlightData => {
+      nextFlightData.connectingGateInfo({}, function(err, data) {
+        resolve({
+          departureGate: data.connecting_flights[0].departure_gate,
+          estimatedDepartureTime: data.connecting_flights[0].departure_time_estimated,
+          scheduledDepartureTime: data.connecting_flights[0].departure_time_scheduled
+        });
+      });
+    });
+  });
+};
