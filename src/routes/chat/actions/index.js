253c3b68d65183488d8ce5b0ca6bdcc5ef847ebc
Ian Ngiaw <ianngiawtingan@gmail.com>
2016-09-10 23:57:09 -0700

    Add more actions and reducers

--- src/routes/chat/actions/index.js
+++ src/routes/chat/actions/index.js
@@ -1,13 +1,47 @@
 import {
   SET_IS_LOADING,
+  RESET_BUTTONS,
   GET_FLIGHT_INFO_SUCCESS,
-  GET_NEXT_FLIGHT_INFO_SUCCESS
+  GET_NEXT_FLIGHT_INFO_SUCCESS,
+  GET_FOOD_ITEMS_SUCCESS,
+  ADD_RESTROOM_CARD,
+  ADD_PANNY_TEXT,
+  ADD_USER_TEXT
 } from '../constants';
 
 import {
+  getPassengerName,
   getCurrentFlightInfo,
   getNextFlightData
 } from '../../../utils/sdk';
+
+export const initialLaunch = () => {
+  return dispatch => {
+    getPassengerName()
+      .then(names => {
+        dispatch(addPannyText(`Hi, ${names.firstName}! Welcome onboard!`));
+        dispatch(getFlightInfo());
+      });
+  };
+};
+
+export const addPannyText = text => {
+  return dispatch => {
+    dispatch({ type: ADD_PANNY_TEXT, payload: { text } });
+  };
+};
+
+export const addUserText = text => {
+  return dispatch => {
+    dispatch({ type: ADD_USER_TEXT, payload: { text } });
+  };
+};
+
+export const addRestroomCard = isFull => {
+  return dispatch => {
+    dispatch({ type: ADD_RESTROOM_CARD, payload: { isFull } });
+  };
+};
 
 export const getFlightInfo = () => {
   return dispatch => {
@@ -33,7 +67,38 @@ export const getNextFlightInfo = () => {
   };
 };
 
+export const getFoodItems = () => {
+  return dispatch => {
+    dispatch({ type: SET_IS_LOADING });
+    setTimeout(() => {
+      dispatch({
+        type: GET_FOOD_ITEMS_SUCCESS,
+        payload: [{
+          name: 'Bolognese Pasta',
+          image: 'http://i.imgur.com/w6B5iX5.jpg'
+        },{
+          name: 'Salmon',
+          image: 'http://i.imgur.com/Eu0unYs.jpg'
+        },{
+          name: 'Salad',
+          image: 'http://i.imgur.com/9k0482t.jpg'
+        }]
+      });
+    }, 200);
+  };
+};
+
+export const resetButtons = () => {
+  return dispatch => {
+    dispatch({ type: RESET_BUTTONS });
+  };
+};
+
 export const actionCreators = {
+  initialLaunch,
+  addPannyText,
+  addUserText,
   getFlightInfo,
-  getNextFlightInfo
+  getNextFlightInfo,
+  getFoodItems
 };
--- src/routes/chat/reducers/index.js
+++ src/routes/chat/reducers/index.js
@@ -1,8 +1,13 @@
 import moment from 'moment';
 import {
   SET_IS_LOADING,
+  RESET_BUTTONS,
+  ADD_RESTROOM_CARD,
+  ADD_PANNY_TEXT,
+  ADD_USER_TEXT,
   GET_FLIGHT_INFO_SUCCESS,
-  GET_NEXT_FLIGHT_INFO_SUCCESS
+  GET_NEXT_FLIGHT_INFO_SUCCESS,
+  GET_FOOD_ITEMS_SUCCESS
 } from '../constants';
 
 const initialState = {
@@ -16,6 +21,38 @@ export const reducer = (state = initialState, action) => {
     case SET_IS_LOADING:
       return Object.assign({}, state, {
         isLoading: true
+      });
+
+    case RESET_BUTTONS:
+      return Object.assign({}, state, {
+        buttons: initialState.buttons
+      });
+
+    case ADD_RESTROOM_CARD:
+      return Object.assign({}, state, {
+        isLoading: false,
+        chatItems: [...state.chatItems, {
+          type: 'RestroomCard',
+          data: action.payload
+        }]
+      });
+
+    case ADD_PANNY_TEXT:
+      return Object.assign({}, state, {
+        isLoading: false,
+        chatItems: [...state.chatItems, {
+          type: 'PannyText',
+          data: action.payload
+        }]
+      });
+
+    case ADD_USER_TEXT:
+      return Object.assign({}, state, {
+        isLoading: false,
+        chatItems: [...state.chatItems, {
+          type: 'UserText',
+          data: action.payload
+        }]
       });
 
     case GET_FLIGHT_INFO_SUCCESS:
@@ -45,6 +82,17 @@ export const reducer = (state = initialState, action) => {
             flightNumber: action.payload.flightNumber,
             departureGate: action.payload.departureGate,
             departureTime: moment(action.payload.departureTime).format('HH:mm')
+          }
+        }]
+      });
+
+    case GET_FOOD_ITEMS_SUCCESS:
+      return Object.assign({}, state, {
+        isLoading: false,
+        chatItems: [...state.chatItems, {
+          type: 'FoodCard',
+          data: {
+            foodItems: action.payload
           }
         }]
       });
