53e09d337cb1260f5b8a8c4429b1087f0026e985
Ian Ngiaw <ianngiawtingan@gmail.com>
2016-09-11 00:30:42 -0700

    Add current flight card transition

--- src/routes/chat/components/CurrentFlightCard.js
+++ src/routes/chat/components/CurrentFlightCard.js
@@ -5,8 +5,10 @@ import {
   Col
 } from 'react-bootstrap';
 import { ActionFlightTakeoff } from 'material-ui/svg-icons';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import classes from './CurrentFlightCard.scss';
+import './CardTransition.scss';
 
 class CurrentFlightCard extends Component {
   static propTypes = {
@@ -29,45 +31,51 @@ class CurrentFlightCard extends Component {
     } = this.props;
 
     return (
-      <Grid className={classes.currentFlightCard}>
-        <Row>
-          <Col xs={3} className={classes.airportLeft}>
-            { origin }
-          </Col>
-          <Col xs={6} className={classes.flightProgress}>
-            <ActionFlightTakeoff
-              color="white"
-              style={{
-                height: '24px',
-                width: '24px',
-                position: 'absolute',
-                top: '10px',
-                left: `${116 * progress + 16}px`
-              }}
-            />
-            <div className={classes.circle}></div>
-            <div className={classes.bar}></div>
-            <div className={classes.circle}></div>
-          </Col>
-          <Col xs={3} className={classes.airportRight}>
-            { destination }
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={4}>
-            <div className={classes.bottomRowLabel}>Est. Arrival</div>
-            <div className={classes.bottomRowValue}>{ estimatedArrival }</div>
-          </Col>
-          <Col xs={4}>
-            <div className={classes.bottomRowLabel}>Time Left</div>
-            <div className={classes.bottomRowValue}>{ timeRemaining }</div>
-          </Col>
-          <Col xs={4}>
-            <div className={classes.bottomRowLabel}>Dist. Left</div>
-            <div className={classes.bottomRowValue}>{ distanceRemaining }</div>
-          </Col>
-        </Row>
-      </Grid>
+      <ReactCSSTransitionGroup
+        transitionName="transitioncard"
+        transitionEnterTimeout={1000}
+        transitionLeaveTimeout={1000}
+      >
+        <Grid className={classes.currentFlightCard}>
+          <Row>
+            <Col xs={3} className={classes.airportLeft}>
+              { origin }
+            </Col>
+            <Col xs={6} className={classes.flightProgress}>
+              <ActionFlightTakeoff
+                color="white"
+                style={{
+                  height: '24px',
+                  width: '24px',
+                  position: 'absolute',
+                  top: '10px',
+                  left: `${116 * progress + 16}px`
+                }}
+              />
+              <div className={classes.circle}></div>
+              <div className={classes.bar}></div>
+              <div className={classes.circle}></div>
+            </Col>
+            <Col xs={3} className={classes.airportRight}>
+              { destination }
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={4}>
+              <div className={classes.bottomRowLabel}>Est. Arrival</div>
+              <div className={classes.bottomRowValue}>{ estimatedArrival }</div>
+            </Col>
+            <Col xs={4}>
+              <div className={classes.bottomRowLabel}>Time Left</div>
+              <div className={classes.bottomRowValue}>{ timeRemaining }</div>
+            </Col>
+            <Col xs={4}>
+              <div className={classes.bottomRowLabel}>Dist. Left</div>
+              <div className={classes.bottomRowValue}>{ distanceRemaining }</div>
+            </Col>
+          </Row>
+        </Grid>
+      </ReactCSSTransitionGroup>
     );
   }
 }
