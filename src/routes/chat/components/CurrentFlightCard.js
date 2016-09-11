c3fca22cb579186c3ea0e8be0b3df685d9009949
Ian Ngiaw <ianngiawtingan@gmail.com>
2016-09-10 18:40:26 -0700

    Add current flight card

---
+++ src/routes/chat/components/CurrentFlightCard.js
@@ -0,0 +1,73 @@
+import React, { Component, PropTypes } from 'react';
+import {
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap';
+import { ActionFlightTakeoff } from 'material-ui/svg-icons';
+
+import classes from './CurrentFlightCard.scss';
+
+class CurrentFlightCard extends Component {
+  static propTypes = {
+    origin: PropTypes.string.isRequired,
+    destination: PropTypes.string.isRequired,
+    estimatedArrival: PropTypes.string.isRequired,
+    timeRemaining: PropTypes.string.isRequired,
+    distanceRemaining: PropTypes.string.isRequired
+  };
+
+  render() {
+    const {
+      origin,
+      destination,
+      estimatedArrival,
+      timeRemaining,
+      distanceRemaining
+    } = this.props;
+
+    return (
+      <Grid>
+        <Row>
+          <Col xs={3} className={classes.airportLeft}>
+            { origin }
+          </Col>
+          <Col xs={6} className={classes.flightProgress}>
+            <ActionFlightTakeoff
+              color="white"
+              style={{
+                height: '24px',
+                width: '24px',
+                position: 'absolute',
+                top: '10px',
+                left: '60px'
+              }}
+            />
+            <div className={classes.circle}></div>
+            <div className={classes.bar}></div>
+            <div className={classes.circle}></div>
+          </Col>
+          <Col xs={3} className={classes.airportRight}>
+            { destination }
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={4}>
+            <div className={classes.bottomRowLabel}>Est. Arrival</div>
+            <div className={classes.bottomRowValue}>{ estimatedArrival }</div>
+          </Col>
+          <Col xs={4}>
+            <div className={classes.bottomRowLabel}>Time Left</div>
+            <div className={classes.bottomRowValue}>{ timeRemaining }</div>
+          </Col>
+          <Col xs={4}>
+            <div className={classes.bottomRowLabel}>Dist. Left</div>
+            <div className={classes.bottomRowValue}>{ distanceRemaining }</div>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+export default CurrentFlightCard;
